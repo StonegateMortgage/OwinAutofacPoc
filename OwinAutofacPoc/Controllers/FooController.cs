20350909ef86d44f440c08ad57806673344e341d
Tom Ginn <tginn@stonegatemtg.com>
2015-05-07 09:08:27 -0500

    Added OWIN, Autofac, and WEB.API 2.2

---
+++ OwinAutofacPoc/Controllers/FooController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OwinAutofacPoc.Api.Controllers
+{
+    public class FooController : ApiController
+    {
+        // GET: api/Foo
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Foo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Foo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Foo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Foo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OwinAutofacPoc/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace OwinAutofacPoc.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // STANDARD WEB API SETUP:
+            
+            // Get your HttpConfiguration. In OWIN, you'll create one
+            // rather than using GlobalConfiguration.
+            var config = GetHttpConfiguration();
+
+            // Run other optional steps, like registering filters,
+            // per-controller-type services, etc., then set the dependency resolver
+            // to be Autofac.
+            var container = GetAutofacContainer();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            // OWIN WEB API SETUP:
+
+            // Register the Autofac middleware FIRST, then the Autofac Web API middleware,
+            // and finally the standard Web API middleware.
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+
+            // Other middleware goes here.....
+
+        }
+
+        private static HttpConfiguration GetHttpConfiguration()
+        {
+            var config = new HttpConfiguration();
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            return config;
+        }
+
+        private static IContainer GetAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+            return container;
+        }
+    }
+}
